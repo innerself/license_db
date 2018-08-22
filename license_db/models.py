dd83cedcc61a96030d5b5f9d97a6c1568780af6b
Sergey Petrin <spetrin@gmail.com>
2018-08-22 22:55:26 +0300

    Add type chioces to License

--- license_db/models.py
+++ license_db/models.py
@@ -4,19 +4,25 @@ from django.db import models
 
 
 class License(models.Model):
-    name = models.CharField(max_length=200)
-    quantity = models.PositiveIntegerField(default=0)
-    expires = models.DateField(default=date.today() + timedelta(days=365))
-    comment = models.CharField(max_length=200, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    TYPES = (
+        ('hardware', 'hardware'),
+        ('software', 'software'),
+    )
+
+    name = models.CharField('name', max_length=200)
+    type = models.CharField('type', max_length=50, choices=TYPES)
+    quantity = models.PositiveIntegerField('quantity', default=0)
+    expires = models.DateField('expires', default=date.today() + timedelta(days=365))
+    comment = models.CharField('comment', max_length=200, blank=True)
+    created = models.DateTimeField('created', auto_now_add=True)
+    modified = models.DateTimeField('modified', auto_now=True)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
 
     @classmethod
     def _generate(cls, number: int) -> None:
-        from random import randint
+        from random import randint, choice
         from mimesis import Business, Text
 
         business = Business()
@@ -25,6 +31,7 @@ class License(models.Model):
         for _ in range(number):
             License.objects.create(
                 name=business.company(),
+                type=choice(cls.TYPES)[0],
                 quantity=randint(0, 10000),
                 comment=text.quote(),
             )
