6843860657120e1176b1a167b87100793b3636b4
Sergey Petrin <spetrin@gmail.com>
2018-08-22 22:56:43 +0300

    Add basic view with all licenses and logo

--- license_db/views.py
+++ license_db/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
+from decouple import config
+
 from .models import License
 
 
 def index(request):
-    licenses = License.objects.all()
-    context = {'licenses': licenses}
-    return render(request, 'license_db/main.html', context)
+    context = {
+        'logo': config('LOGO'),
+        'licenses': dict(),
+    }
 
+    lic_types = set([
+        value
+        for item in License.objects.values('type')
+        for _, value in item.items()
+    ])
+
+    for lic_type in lic_types:
+        context['licenses'][lic_type] = License.objects.filter(
+            type=lic_type,
+        )
+
+    return render(request, 'license_db/main.html', context)
